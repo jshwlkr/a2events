525a18f9abd572e8b8e826d2bd4c7f8c75b74f36
Joshua Walker <joshua@snowandbones.com>
2015-06-08 03:23:36 -0400

    starting on front-end

---
+++ js/a2events.js
@@ -0,0 +1,26 @@
+$(document).ready( function() {
+
+    $.ajaxSetup({ scriptCharset: "utf-8" , contentType: "application/json; charset=utf-8"});
+    $.getJSON('data.json', function(json) {
+
+      $('#calendar').clndr({
+        events: json,
+        template: $('#template').html(),
+
+  clickEvents: {
+    click: function(target) {
+      console.log(target);
+    },
+    onMonthChange: function(month) {
+      console.log('you just went to ' + month.format('MMMM, YYYY'));
+    }
+  },
+  doneRendering: function() {
+    console.log('this would be a fine place to attach custom event handlers.');
+  }
+});
+
+
+
+    });
+});
