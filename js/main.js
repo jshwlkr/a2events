a9af5e629d233f78b8847190f0aa041a295bc6e1
Joshua Walker <joshua@snowandbones.com>
2015-06-28 18:57:00 -0400

    changes?

--- js/main.js
+++ js/main.js
@@ -5,13 +5,19 @@
   app.controller('CalendarController', function($scope, $http) {
     $http.get('../data.json')
          .success(function(data, status, headers, config) {
+          angular.forEach(data, function(value, key) {
+            value['date'] = Date.parse(value['date']);
+          });
       $scope.events = data;
+
       console.log('sucess');
     })
-         .error(function(data, status, headers, config) {
+    .error(function(data, status, headers, config) {
       console.log('error');
       console.log(status);
     });
   });
+
+
 
 })();
