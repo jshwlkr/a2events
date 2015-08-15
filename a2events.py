3e5bd0af3b6d36b00826fce2b0bea896d3325d97
Joshua Walker <joshua@snowandbones.com>
2015-08-14 20:22:12 -0400

    directories shenanigans

--- a2events.py
+++ a2events.py
@@ -84,10 +84,10 @@ def facebook_fetch(token):
 
 def to_github(event_list):
     #ghpages = Repo.clone_from('https://github.com/jshwlkr/a2events.git', 'temp_repo')
-    os.system("git clone -v git@github.com:jshwlkr/a2events.git temp_repo")
-    ghpages = Repo("temp_repo")
+    os.system("git clone -v git@github.com:jshwlkr/a2events.git")
+    ghpages = Repo("a2events")
     ghpages.git.checkout('gh-pages')
-    with open('temp_repo/data.json', 'w') as outfile:
+    with open('a2events/data.json', 'w') as outfile:
         json.dump(event_list, outfile)
 
     print "json"
