1da184dd99af33b47ab13cad7490d2df07e0c926
Joshua Walker <joshua@snowandbones.com>
2015-08-15 23:31:19 -0400

    testing heroku dirs

--- a2events.py
+++ a2events.py
@@ -85,6 +85,7 @@ def facebook_fetch(token):
 def to_github(event_list):
     #ghpages = Repo.clone_from('https://github.com/jshwlkr/a2events.git', 'temp_repo')
     os.system("git clone -v git@github.com:jshwlkr/a2events.git")
+    os.system("ls")
     ghpages = Repo("a2events")
     ghpages.git.checkout('gh-pages')
     with open('a2events/data.json', 'w') as outfile:
